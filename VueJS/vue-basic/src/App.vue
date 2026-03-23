a6bc30533993dcdeb1983bd1397d3cd910415522
dev05 <dev05@dev.co-mit.com>
2026-03-23 10:15:46 +0700

    Data Binding

--- VueJS/vue-basic/src/App.vue
+++ VueJS/vue-basic/src/App.vue
@@ -1,10 +1,52 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
 
 const name = ref('')
+const email = ref('')
+const age = ref(0)
+const color = ref('#000000')
+const isAgree = ref(false)
+
+function submit() {
+  alert('Submitted!')
+}
 </script>
 
 <template>
-  <input v-model="name" />
-  <p>Hello {{ name }}</p>
+  <div>
+    <h2>Profile Form</h2>
+
+    <!-- v-model -->
+    <input v-model="name" placeholder="Name" />
+    <input v-model="email" placeholder="Email" />
+    <input type="number" v-model="age" />
+    <input type="color" v-model="color" />
+
+    <!-- checkbox -->
+    <label>
+      <input type="checkbox" v-model="isAgree" />
+      Agree terms
+    </label>
+
+    <!-- interpolation -->
+    <h3 :style="{ color: color }">
+      Xin chào {{ name || '...' }}
+    </h3>
+
+    <p>Email: {{ email }}</p>
+    <p>Age: {{ age }}</p>
+
+    <!-- condition + class -->
+    <p :class="{ warning: !isAgree }">
+      {{ isAgree ? 'Đã đồng ý' : 'Bạn chưa đồng ý!' }}
+    </p>
+
+    <!-- button -->
+    <button 
+      :disabled="!email || !isAgree"
+      @click="submit"
+    >
+      Submit
+    </button>
+  </div>
 </template>
